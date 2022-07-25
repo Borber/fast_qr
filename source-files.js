3536a146fa75ccde4521ae8e0d2bb5f7b1273e2b
erwanvivien <erwanvivien@users.noreply.github.com>
2022-07-25 17:03:41 +0000

    deploy: d7286c27f3b7a73ec05bc44bd4e93e6b7ecd4fb6

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["fast_qr"] = {"name":"","dirs":[{"name":"convert","files":["mod.rs","svg.rs"]}],"files":["bitstring.rs","datamasking.rs","default.rs","ecl.rs","encode.rs","hardcode.rs","helpers.rs","lib.rs","module.rs","placement.rs","polynomials.rs","qr.rs","score.rs","version.rs"]};
+sourcesIndex["fast_qr"] = {"name":"","dirs":[{"name":"convert","files":["mod.rs","svg.rs"]}],"files":["compact.rs","datamasking.rs","default.rs","ecl.rs","encode.rs","hardcode.rs","helpers.rs","lib.rs","module.rs","placement.rs","polynomials.rs","qr.rs","score.rs","version.rs"]};
 createSourceSidebar();
